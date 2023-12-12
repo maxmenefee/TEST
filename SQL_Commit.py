708e3308d435b4de4b9ff3d721a2956942c454c9
maxmenefee <147845697+maxmenefee@users.noreply.github.com>
2023-12-12 17:11:41 -0600

    Update SQL_Commit.py

--- SQL_Commit.py
+++ SQL_Commit.py
@@ -4,30 +4,37 @@ import pyodbc
 # Initialize connection.
 # Uses st.cache_resource to only run once.
 @st.cache_resource
-def init_connection():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + st.secrets["server"]
-        + ";DATABASE="
-        + st.secrets["database"]
-        + ";UID="
-        + st.secrets["username"]
-        + ";PWD="
-        + st.secrets["password"]
-    )
+# def init_connection():
+#     return pyodbc.connect(
+ #        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+ #        + st.secrets["server"]
+  #      + ";DATABASE="
+  #      + st.secrets["database"]
+  #      + ";UID="
+  #      + st.secrets["username"]
+  #      + ";PWD="
+  #      + st.secrets["password"]
+ #   )
 
-conn = init_connection()
+#conn = init_connection()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+#@st.cache_data(ttl=600)
+#def run_query(query):
+#    with conn.cursor() as cur:
+#        cur.execute(query)
+#        return cur.fetchall()
 
-rows = run_query("SELECT * from employee;")
+#rows = run_query("SELECT * from employee;")
 
 # Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+#for row in rows:
+server = st.secrets["server"]
+database = st.secrets["database"]
+username = st.secrets["username"]
+password = st.secrets["password"]
+
+string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=" + server +";DATABASE=" + database + ";UID=" + username + ";PWD=" + password
+
+st.write(string)
