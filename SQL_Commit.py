ba480e687c431233a7c35edb0fff524a80a42570
maxmenefee <147845697+maxmenefee@users.noreply.github.com>
2023-12-12 17:27:05 -0600

    Update SQL_Commit.py

--- SQL_Commit.py
+++ SQL_Commit.py
@@ -3,35 +3,34 @@ import pyodbc
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-#@st.cache_resource
-#def init_connection():
-#    return pyodbc.connect(
-#        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-#        + st.secrets["server"]
-#        + ";DATABASE="
-#        + st.secrets["database"]
-#        + ";UID="
-#        + st.secrets["username"]
-#        + ";PWD="
-#        + st.secrets["password"]
-#    )
+@st.cache_resource
+def init_connection():
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"]
+    )
+conn = init_connection()
 
-#conn = init_connection()
+ #Perform query.
+ #Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-#@st.cache_data(ttl=600)
-#def run_query(query):
-#    with conn.cursor() as cur:
-#        cur.execute(query)
-#        return cur.fetchall()
+rows = run_query("SELECT * from employee;")
 
-#rows = run_query("SELECT * from employee;")
+ #Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
 
-# Print results.
-#for row in rows:
-#    st.write(f"{row[0]} has a :{row[1]}:")
+#string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=" + st.secrets["server"] + ";DATABASE=" + st.secrets["database"] + ";UID=" + st.secrets["username"] + ";PWD=" + st.secrets["password"]
 
-string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=" + st.secrets["server"] + ";DATABASE=" + st.secrets["database"] + ";UID=" + st.secrets["username"] + ";PWD=" + st.secrets["password"]
-
-st.write(string)
+#st.write(string)
